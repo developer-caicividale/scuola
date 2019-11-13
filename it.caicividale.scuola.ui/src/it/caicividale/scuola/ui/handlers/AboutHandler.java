55b9ea678217271ba194867e1496e06590107ba3
Marco Knezevich <development.caicividale@gmail.com>
2019-11-13 17:17:30 +0100

    introdotto supporto al css

--- it.caicividale.scuola.ui/src/it/caicividale/scuola/ui/handlers/AboutHandler.java
+++ it.caicividale.scuola.ui/src/it/caicividale/scuola/ui/handlers/AboutHandler.java
@@ -3,12 +3,10 @@ package it.caicividale.scuola.ui.handlers;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.widgets.Shell;
 
-import it.caicividale.scuola.service.ServiceManager;
-
 public class AboutHandler {
-	@Execute
-	public void execute(Shell shell) {
-		// MessageDialog.openInformation(shell, "About", "Eclipse 4 RCP Application");
-		ServiceManager.getInstance().popolaCorsi();
-	}
+    @Execute
+    public void execute(Shell shell) {
+	// MessageDialog.openInformation(shell, "About", "Eclipse 4 RCP Application");
+	// ServiceManager.getInstance().popolaCorsi();
+    }
 }
